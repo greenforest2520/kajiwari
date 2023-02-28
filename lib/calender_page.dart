e23b23ee67a9dfef4d6b9b841511c7a59f6d8712
greenforest2520 <greenforest2520@gmail.com>
2023-02-28 16:31:00 +0900

    first comitt

---
+++ lib/calender_page.dart
@@ -0,0 +1,107 @@
+import 'dart:collection';
+
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+class Calender extends StatefulWidget {
+  @override
+  State<Calender> createState() => _CalenderState();
+}
+
+class _CalenderState extends State<Calender> {
+  Map<DateTime, List> _eventsList = {};
+
+  DateTime _focused = DateTime.now();
+  DateTime? _selected;
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  int getHashCode(DateTime key) {
+    return key.day * 1000000 + key.month * 10000 + key.year;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _selected = _focused;
+    _eventsList = {
+      DateTime.now().subtract(Duration(days: 2)): ['Test ei', 'Test Bi'],
+      DateTime.now(): ['Test Ci', 'Test Di', 'Test E', 'Test ehu'],
+    };
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final _events = LinkedHashMap<DateTime, List>(
+      equals: isSameDay,
+      hashCode: getHashCode,
+    )..addAll(_eventsList);
+
+    List getEvent(DateTime day) {
+      return _events[day] ?? [];
+    }
+
+    return Expanded(
+      child: Column(children: [
+        TableCalendar(
+          firstDay: DateTime.utc(2022, 4, 1),
+          lastDay: DateTime.utc(2025, 12, 31),
+          headerStyle: HeaderStyle(
+            formatButtonVisible: false,
+          ),
+          selectedDayPredicate: (day) {
+            return isSameDay(_selected, day);
+          },
+          onDaySelected: (selected, focused) {
+            if (!isSameDay(_selected, selected)) {
+              setState(() {
+                _selected = selected;
+                _focused = focused;
+              });
+            }
+          },
+          focusedDay: _focused,
+          calendarBuilders: CalendarBuilders(
+            markerBuilder: (context, date, events) {
+              if (events.isNotEmpty) {
+                return _buildEventsMarker(date, events);
+              }
+            },
+          ),
+        ),
+        ListView(
+          shrinkWrap: true,
+          children: getEvent(_selected!)
+              .map((event) => ListTile(
+                    title: Text(event.toString()),
+                  ))
+              .toList(),
+        )
+      ]),
+    );
+  }
+
+  Widget _buildEventsMarker(DateTime date, List events) {
+    return Positioned(
+      right: 5,
+      bottom: 5,
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 300),
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: Colors.red[300],
+        ),
+        width: 16.0,
+        height: 16.0,
+        child: Center(
+          child: Text(
+            '${events.length}',
+            style: TextStyle().copyWith(
+              color: Colors.white,
+              fontSize: 12.0,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,130 @@
+import 'dart:collection';
+
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+import 'calender_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyWidget(),
+    );
+  }
+}
+
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  int _selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(
+        children: [
+          NavigationRail(
+            extended: true,
+            minExtendedWidth: 200,
+            backgroundColor: Colors.grey,
+            elevation: 5,
+            useIndicator: true,
+            indicatorColor: Colors.grey[300],
+            destinations: const [
+              NavigationRailDestination(
+                icon: Icon(Icons.mood),
+                label: Text('Home'),
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.calendar_month),
+                label: Text('Calendar'),
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.casino),
+                label: Text('roulette'),
+              ),
+            ],
+            selectedIndex: _selectedIndex,
+            onDestinationSelected: (index) {
+              setState(() {
+                _selectedIndex = index;
+              });
+            },
+          ),
+          MainContents(index: _selectedIndex)
+        ],
+      ),
+    );
+  }
+}
+
+class MainContents extends StatelessWidget {
+  const MainContents({super.key, required this.index});
+
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    switch (index) {
+      case 1:
+        return Calender();
+      case 2:
+        return Expanded(
+          child: ColoredBox(
+            color: Colors.green[200]!,
+            child: const Center(
+              child: Text('Friends'),
+            ),
+          ),
+        );
+      default:
+        return Profile();
+    }
+  }
+}
+
+class Profile extends StatelessWidget {
+  const Profile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ColoredBox(
+        color: Colors.white30,
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 50,
+              ),
+              Text("userNameさん"),
+              SizedBox(
+                height: 50,
+              ),
+              CircleAvatar(
+                minRadius: 50,
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Text("今日のタスク"),
+              ListTile()
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
